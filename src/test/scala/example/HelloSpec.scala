ab8a65ec0c486a5cba34dcda05ff82d728e48526
Jarno Rajala <jarno.rajala@schibsted.com>
2021-06-07 09:03:32 +0300

    more tests

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -11,6 +11,17 @@ class HelloSpec extends AnyFlatSpec with Matchers {
     Hello.sqrt(2) should be(1.41 +- 0.01)
   }
 
+  "Squares of roots" should "be square roots" in {
+    val numbers : Seq[Double] = Seq(1,2,3,4,5,6,7,123)
+    val epsilon = 0.01
+
+    for (n <- numbers) {
+      val y = Hello.sqrt(n)
+
+      (y*y) should be (n +- epsilon)
+    }
+  }
+
   "Code" should "be 100% correct" in {
     val hash = Utils.hashFile("src/main/scala/example/Hello.scala")
     val expected = "P3aQD5Mjwp0hx/4A1Ri4CA=="
