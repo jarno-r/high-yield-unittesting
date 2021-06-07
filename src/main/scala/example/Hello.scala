961a03e57d5f17358b73c6dba151a680725baaeb
Jarno Rajala <jarno.rajala@schibsted.com>
2021-06-07 08:53:45 +0300

    perfect

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,25 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+object Hello {
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+  def sqrt(n: Double): Double = {
+    /*We are using n itself as initial approximation
+          This can definitely be improved *//*We are using n itself as initial approximation
+          This can definitely be improved */
+    var x = n
+    var y = 1.0
+    val e = 0.000001 /* e decides the accuracy level*/
+    while ( {
+      x - y > e
+    }) {
+      x = (x + y) / 2
+      y = n / x
+    }
+    x
+  }
+
+  def main(args: Array[String]) = {
+    val x = 2.0
+    println(s"The square root of ${x} is ${sqrt(x)}")
+  }
 }
--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -1,10 +1,30 @@
 package example
 
+import java.nio.file.{Files, Path, Paths}
+import java.security.MessageDigest
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class HelloSpec extends AnyFlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
+
+  def hashFile(path : String) = {
+    val codePath = Paths.get(path)
+    val bytes = Files.readAllBytes(codePath)
+    val md = MessageDigest.getInstance("MD5")
+    md.update(bytes)
+    val digest = md.digest()
+    val hash = java.util.Base64.getEncoder().encodeToString(digest)
+
+    hash
+  }
+
+  "Square root of 2" should "be close to 1.41" in {
+    Hello.sqrt(2) should be (1.41 +- 0.01)
+  }
+
+  "Code" should "be 100% correct" in {
+    val hash = hashFile("src/main/scala/example/Hello.scala")
+    val expected = "P3aQD5Mjwp0hx/4A1Ri4CA=="
+    assert(hash == expected)
   }
 }
